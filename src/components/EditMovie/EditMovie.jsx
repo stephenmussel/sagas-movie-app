420f9869280130842c7a98aebd3c1d84b6efc7d1
phaydara <phaydara@gmail.com>
2022-06-05 17:03:10 -0500

    captured form inputs

--- src/components/EditMovie/EditMovie.jsx
+++ src/components/EditMovie/EditMovie.jsx
@@ -1,14 +1,21 @@
 import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 
 function EditMovie() {
 
     const details = useSelector(store => store.details)
     const history = useHistory();
+    
+    const [movieEdits, setMovieEdits] = useState({
+        title: "",
+        description: ""
+    });
 
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log('clicked save!');
+        console.log('movieEdits:', movieEdits);
     }
 
     return (
@@ -19,11 +26,15 @@ function EditMovie() {
                     type="text"
                     placeholder="Title"
                     style={{ marginBottom: 5 }}
+                    value={movieEdits.title}
+                    onChange={(event) => setMovieEdits({...movieEdits, title: event.target.value })}
                 /><br />
                 <textarea 
                     type="text"
                     placeholder="Description"
                     style={{ marginBottom: 5 }}
+                    value={movieEdits.description}
+                    onChange={(event) => setMovieEdits({...movieEdits, description: event.target.value })}
                 /><br />
                 <input type="submit" value="Save" style={{ marginRight: 5 }} />
                 <button onClick={() => history.push(`/details/${details.id}`)}>Cancel</button>
@@ -45,6 +56,5 @@ Edit page should show:
 
 The edit page should have the buttons:
 
-- `Cancel` button, which should bring the user to the Details Page
 - `Save` button, which should update the title and description in the database and bring the user to the Details Page
  */
