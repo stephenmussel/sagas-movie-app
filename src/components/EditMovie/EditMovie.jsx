4d52474261f8b6abaacb422704f8f650464aea12
phaydara <phaydara@gmail.com>
2022-06-05 15:53:15 -0500

    acutally added client side route for edit movie view

---
+++ src/components/EditMovie/EditMovie.jsx
@@ -0,0 +1,25 @@
+function EditMovie() {
+    return(
+        <>
+            <div>Edit Page...</div>
+        </>
+    )
+}
+
+export default EditMovie;
+
+// TODO:
+
+/**
+- Add to the detail page an edit button that brings the user to the edit page.
+
+Edit page should show:
+
+- an input field (for changing the movie title), for the selected movie.
+- a textarea (for changing the movie description)
+
+The edit page should have the buttons:
+
+- `Cancel` button, which should bring the user to the Details Page
+- `Save` button, which should update the title and description in the database and bring the user to the Details Page
+ */
