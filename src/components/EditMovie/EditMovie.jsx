b99a6ddf17b33df5ac7f7c4d97ab5f20cfca3071
phaydara <phaydara@gmail.com>
2022-06-06 10:51:33 -0500

    display current details in input fields and added comments to review

--- src/components/EditMovie/EditMovie.jsx
+++ src/components/EditMovie/EditMovie.jsx
@@ -7,48 +7,98 @@ function EditMovie() {
     const details = useSelector(store => store.details)
     const history = useHistory();
     const dispatch = useDispatch();
-    
-    const [movieEdits, setMovieEdits] = useState({
-        id: details.id,
-        title: "",
-        description: ""
-    });
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    // const [movieEdits, setMovieEdits] = useState({
+    //     id: details.id,
+    //     title: "",
+    //     description: ""
+    // });
 
     const showCurrentDetails = () => {
         console.log('in showCurrentDetails');
+
+        details.title && setTitle(details.title); // shows current title in input field
+        details.description && setDescription(details.description); // shows current description in input field
+    }
+
+    const editedDetails = {
+        id: details.id,
+        title: title,
+        description: description
     }
 
     useEffect(() => {
-        console.log('in EditMovie useEffect');
         showCurrentDetails();
+        dispatch({ type: 'FETCH_DETAILS'}); // GETs updated details after edits saved
     }, [])
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     console.log('clicked save!');
+    //     console.log('movieEdits:', movieEdits);
+
+    //     dispatch({ type: 'UPDATE_MOVIE', payload: movieEdits});
+    //     history.push(`/details/${movieEdits.id}`)
+    // }
 
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log('clicked save!');
-        console.log('movieEdits:', movieEdits);
+        console.log('editedDetails:', editedDetails);
 
-        dispatch({ type: 'UPDATE_MOVIE', payload: movieEdits});
-        history.push(`/details/${movieEdits.id}`)
+        dispatch({ type: 'UPDATE_MOVIE', payload: editedDetails }); // sends updated details to saga
+        history.push(`/details/${details.id}`);
     }
+
+    // return (
+    //     <div>
+    //         <h1>Edit: <em>{details.title}</em></h1>
+    //         <p>title: {title}</p>
+    //         <form onSubmit={handleSubmit}>
+    //             <input 
+    //                 type="text"
+    //                 placeholder="Title"
+    //                 style={{ marginBottom: 5 }}
+    //                 // value={movieEdits.title}
+    //                 value={title}
+    //                 onChange={(event) => setMovieEdits({...movieEdits, title: event.target.value })}
+    //             /><br />
+    //             <textarea 
+    //                 type="text"
+    //                 placeholder="Description"
+    //                 style={{ marginBottom: 5 }}
+    //                 value={movieEdits.description}
+    //                 onChange={(event) => setMovieEdits({...movieEdits, description: event.target.value })}
+    //             /><br />
+    //             <input type="submit" value="Save" style={{ marginRight: 5 }} />
+    //             <button onClick={() => history.push(`/details/${details.id}`)}>Cancel</button>
+    //         </form>
+    //     </div>
+    // )
 
     return (
         <div>
             <h1>Edit: <em>{details.title}</em></h1>
+            {/* <p>title: {title}</p> */}
             <form onSubmit={handleSubmit}>
-                <input 
+                <input
                     type="text"
                     placeholder="Title"
                     style={{ marginBottom: 5 }}
-                    value={movieEdits.title}
-                    onChange={(event) => setMovieEdits({...movieEdits, title: event.target.value })}
+                    value={title}
+                    // defaultValue={details.title}
+                    onChange={(event) => setTitle(event.target.value)}
                 /><br />
-                <textarea 
+                <textarea
                     type="text"
                     placeholder="Description"
                     style={{ marginBottom: 5 }}
-                    value={movieEdits.description}
-                    onChange={(event) => setMovieEdits({...movieEdits, description: event.target.value })}
+                    value={description}
+                    // defaultValue={details.description}
+                    onChange={(event) => setDescription(event.target.value)}
                 /><br />
                 <input type="submit" value="Save" style={{ marginRight: 5 }} />
                 <button onClick={() => history.push(`/details/${details.id}`)}>Cancel</button>
@@ -58,3 +108,5 @@ function EditMovie() {
 }
 
 export default EditMovie;
+
+// REVIEW: using value vs defaultValue?
