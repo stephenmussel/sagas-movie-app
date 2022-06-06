bd590f440f5e94f4d6182cb6c09e951d0bcb462f
phaydara <phaydara@gmail.com>
2022-06-06 15:03:54 -0500

    added header.css and styled

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <div id="header" >
+            <h1>The Earls of Risk</h1>
+        </div>
+    );
+};
+
+export default Header;
