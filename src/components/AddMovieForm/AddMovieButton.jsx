031a26e06bcacc54602187af0b2df642a9273776
phaydara <phaydara@gmail.com>
2022-06-07 11:02:34 -0500

    styled add movie button

--- src/components/AddMovieForm/AddMovieButton.jsx
+++ src/components/AddMovieForm/AddMovieButton.jsx
@@ -1,3 +1,6 @@
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import Stack from '@mui/material/Stack';
 import { useHistory } from 'react-router-dom';
 
 function AddMovieButton() {
@@ -5,9 +8,15 @@ function AddMovieButton() {
     const history = useHistory();
 
     return (
-        <div>
-            <button onClick={() => history.push('/add-movie')}>Add Movie</button>
-        </div>
+        <Stack direction="row" spacing={2}>
+            <Button 
+                onClick={() => history.push('/add-movie')} 
+                variant="contained" 
+                startIcon={<AddIcon />}
+            >
+                Add Movie
+            </Button>
+        </Stack>
     )
 }
 
