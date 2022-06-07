9d97b370cafee37d7e577d145741e05570d95075
phaydara <phaydara@gmail.com>
2022-06-07 10:52:08 -0500

    moved AddMovie button to component

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,41 +2,20 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
 import MovieItem from '../MovieItem/MovieItem';
-import { useHistory } from 'react-router-dom';
+import AddMovieButton from '../AddMovieForm/AddMovieButton';
 
 function MovieList() {
 
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
-    const history = useHistory();
 
     useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
+        dispatch({ type: 'FETCH_MOVIES' }); // gets movie list on page load
     }, []);
-
-    // return (
-    //     <main>
-    //         <div>
-    //             <button onClick={() => history.push('/add-movie')}>Add Movie</button>
-    //         </div>
-    //         {/* <h1>MovieList</h1> */}
-    //         <section className="movies">
-    //             {movies.map(movie => {
-    //                 return (
-    //                     <div key={movie.id} >
-    //                         <MovieItem movie={movie} />
-    //                     </div>
-    //                 );
-    //             })}
-    //         </section>
-    //     </main>
-    // );
 
     return (
         <main>
-            <div>
-                <button onClick={() => history.push('/add-movie')}>Add Movie</button>
-            </div>
+            <AddMovieButton />
             {/* <h1>MovieList</h1> */}
             <section className="movies">
                 {movies.map(movie => {
